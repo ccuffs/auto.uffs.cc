1f4300a8293d13a3d37e13c1357ad938875a355a
Fernando Bevilacqua <dovyski@gmail.com>
2020-09-07 12:10:46 -0300

    Estrutura inicial

---
+++ n8n-start.sh
@@ -0,0 +1,2 @@
+nohup /home/ccuffssys/auto.uffs.cc/n8n.js >> /home/ccuffssys/auto.uffs.cc/n8n.log 2>&1 &
+echo $! > /home/ccuffssys/auto.uffs.cc/n8n.pid
---
+++ n8n-stop.sh
@@ -0,0 +1,2 @@
+kill -9 `cat /home/ccuffssys/auto.uffs.cc/n8n.pid`
+rm /home/ccuffssys/auto.uffs.cc/n8n.pid
